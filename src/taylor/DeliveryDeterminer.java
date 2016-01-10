cf533e2527f741de6b9bb671e769358f2454de68
Taylor Moon <therealtaylormoon@gmail.com>
2016-01-10 13:38:01 -0600

    Revert "Testing units and modularized code"
    This reverts commit 8d9ec70157b91f7c39e941f777930be813db744c.

--- src/taylor/DeliveryDeterminer.java
+++ src/taylor/DeliveryDeterminer.java
@@ -65,8 +65,6 @@ public class DeliveryDeterminer {
 			case 'v' :
 				newY--;
 				break;
-			default :
-				throw new IllegalArgumentException("Input command \"" + command + "\" isn't supported.");
 		}
 		santa.setX(newX);
 		santa.setY(newY);
